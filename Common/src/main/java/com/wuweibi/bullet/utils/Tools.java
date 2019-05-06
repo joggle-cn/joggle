2e0b73f91c12b40a99faf42a5b92f40ab1aca841
wuwei <wuwei@byitgroup.com>
2019-05-06 13:48:13 +0800

    新增IP地址采集，可通过后端查看

--- Common/src/main/java/com/wuweibi/bullet/utils/Tools.java
+++ Common/src/main/java/com/wuweibi/bullet/utils/Tools.java
@@ -1,9 +1,11 @@
 package com.wuweibi.bullet.utils;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Enumeration;
 
 /**
  * 工具类
@@ -31,9 +33,9 @@ public class Tools {
 	 * @return
 	 */
 	public static String getIp() {
-		InetAddress addr = null;
+		InetAddress addr ;
 		try {
-			addr = InetAddress.getLocalHost();
+            addr = getLocalHostLANAddress();
 		} catch (UnknownHostException e) {
 			return "";
 		}
@@ -43,4 +45,44 @@ public class Tools {
 //		String hostname = addr.getHostName();
 //		System.out.println("Local host name: "+hostname);
 	}
+
+    // 正确的IP拿法，即优先拿site-local地址
+    private static  InetAddress getLocalHostLANAddress() throws UnknownHostException {
+        try {
+            InetAddress candidateAddress = null;
+            // 遍历所有的网络接口
+            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements();) {
+                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+                // 在所有的接口下再遍历IP
+                for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
+                    InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
+                    if (!inetAddr.isLoopbackAddress()) {// 排除loopback类型地址
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了
+                            return inetAddr;
+                        } else if (candidateAddress == null) {
+                            // site-local类型的地址未被发现，先记录候选地址
+                            candidateAddress = inetAddr;
+                        }
+                    }
+                }
+            }
+            if (candidateAddress != null) {
+                return candidateAddress;
+            }
+            // 如果没有发现 non-loopback地址.只能用最次选的方案
+            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+            if (jdkSuppliedAddress == null) {
+                throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
+            }
+            return jdkSuppliedAddress;
+        } catch (Exception e) {
+            UnknownHostException unknownHostException = new UnknownHostException(
+                    "Failed to determine LAN address: " + e);
+            unknownHostException.initCause(e);
+            throw unknownHostException;
+        }
+    }
+
+
 }
---
+++ Common/src/test/java/ToolsTest.java
@@ -0,0 +1,24 @@
+/**
+ * Created by marker on 2018/7/6.
+ */
+
+import com.wuweibi.bullet.utils.Tools;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author marker
+ * @create 2018-07-06 21:15
+ **/
+public class ToolsTest {
+
+
+    @Test
+    public void test() throws IOException, InterruptedException {
+
+        System.out.println(Tools.getIp());
+    }
+
+
+}
