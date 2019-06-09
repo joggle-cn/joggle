5311cd7b4521a295568c5f41661167f05f025311
wuwei <wuwei@byitgroup.com>
2019-06-09 16:01:55 +0800

    停止优化

--- databases/update.sql
+++ databases/update.sql
@@ -10,7 +10,7 @@ ADD COLUMN `bind_tls`  int(1) NULL AFTER `hostname`;
 
 
 
--- 2019年6月4日 更新
+-- 2019年6月4日 更新(线上已更新)
 
 ALTER TABLE `t_device_online` CHANGE COLUMN `deviceId` `deviceNo` varchar(255) NOT NULL COMMENT '设备编号';
 ALTER TABLE `t_device_online` ADD COLUMN `mac_addr` varchar(20) COMMENT 'Mac地址' AFTER `intranetIp`;
@@ -22,7 +22,7 @@ ALTER TABLE `t_device` CHANGE COLUMN `intranetIp` `intranet_ip` varchar(30) DEFA
 
 
 
--- 2019年6月4日
+-- 2019年6月4日 (线上已更新)
 ALTER TABLE `t_user` ADD COLUMN `icon` varchar(255) COMMENT '头像' AFTER `agree`;
 ALTER TABLE `t_device_mapping` ADD COLUMN `status` int COMMENT '映射状态 （1、启用；0、停用)' AFTER `tls_key`, AUTO_INCREMENT=6;
 update t_device_mapping set status = 1
