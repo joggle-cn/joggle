fbae531f95d86a0636d84cd342671e681a18db94
marker <wuwei@rollbankpay.com>
2021-02-19 22:14:20 +0800

    sql

--- bullet-server/src/main/resources/static/js/faceinner.ajax.js
+++ bullet-server/src/main/resources/static/js/faceinner.ajax.js
@@ -7,7 +7,7 @@
  */
 
 // 读取服务器地址
-let server = 'http://joggle.cn:8081';
+let server = 'http://localhost:8081';
 if(window.SERVER_URL && window.SERVER_URL != '${url}'){ // 如果配置了
     server = window.SERVER_URL;
 }
