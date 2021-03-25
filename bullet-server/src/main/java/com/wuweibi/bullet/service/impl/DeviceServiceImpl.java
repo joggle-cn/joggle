97ce956c4cd87c54f2b21f7369cc1bf0169db144
marker <wuwei@rollbankpay.com>
2021-03-25 14:12:41 +0800

    优化网络唤醒消息

--- bullet-server/src/main/java/com/wuweibi/bullet/service/impl/DeviceServiceImpl.java
+++ bullet-server/src/main/java/com/wuweibi/bullet/service/impl/DeviceServiceImpl.java
@@ -56,13 +56,9 @@ public class DeviceServiceImpl extends ServiceImpl<DeviceMapper, Device> impleme
 
     @Override
     public void wakeUp(Long userId, String mac) {
-
         // 查询所有设备是因为大部分用户的设备数量并不多。
         List<Device> deviceList = getListByUserId(userId);
-        MsgWOL msg = new MsgWOL();
-        msg.setMac(mac);
-
-
+        MsgWOL msg = new MsgWOL(mac);
         deviceList.forEach(item ->{
             String deviceNo = item.getDeviceNo();
             coonPool.boradcast(deviceNo, msg);
