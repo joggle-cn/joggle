818f4e09024dff3e5b8aa8f5977654f17f1b40eb
marker <admin@wuweibi.com>
2022-10-27 14:00:24 +0800

    V1.3.x CI加入管理代码

--- joggle-server/src/main/java/com/wuweibi/bullet/config/WebMvcConfig.java
+++ joggle-server/src/main/java/com/wuweibi/bullet/config/WebMvcConfig.java
@@ -57,6 +57,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/manager/").setViewName("forward:/manager/index.html");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
         WebMvcConfigurer.super.addViewControllers(registry);
     }
